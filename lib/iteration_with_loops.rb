8435b21a79dce637e3cbab514aa20cd793d78210
Brian Cook <brybcook@gmail.com>
2021-02-05 17:54:18 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -11,9 +11,9 @@ while row_index < src.count do
     if src[row_index][element_index].class==String
     string=src[row_index][element_index]
   end
-    element_index+=1
     word=string+space
     sentence+=word
+    element_index+=1
 end
   row_index+=1
 end
